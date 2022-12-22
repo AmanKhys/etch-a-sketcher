50e83e1e1162ee22eb01bf78879a5d24ce5f9ee2
AmanKhys <amankhys2112@gmail.com>
2022-12-22 20:56:09 +0530

    register changin div id's in an array

--- script.js
+++ script.js
@@ -5,13 +5,33 @@ const header = document.getElementById('header');
 
 
 let count =16;
-
+let moveCount = 0;
+let changeInt = 0;
+const changedPixels = [];
 //change pixel style function
 function changePixelStyle(e){
     console.log(`entered styles`);
     const pixel = e.target;
     pixel.classList.add('pixel-move-style');
+    console.log(moveCount);
+
+    changedPixels[moveCount] = pixel.id;
+    console.log(changedPixels);
+    changeInt = changedPixels.length;
+    console.log(changeInt);
+
+//return pixel style
+
+    if(changeInt>10){
+        let changingPixelInt = changedPixels[0];
+        const changingPixel = document.getElementById(`${changingPixelInt}`);
+        changingPixel.classList.remove('pixel-move-style');
+        
+    }
+    moveCount++;
+
 }
+
 
 
 //make pixel container function
@@ -23,12 +43,14 @@ function makeContainer(pixelCount){
         for(let j = 0; j<pixelCount; j++){
             const pixel = document.createElement('div');
             pixel.classList.add('pixel-style');
+            pixel.setAttribute(`id`,`${i*10 + j}`);
             pixelRow.appendChild(pixel);
-            pixel.addEventListener('mouseover', (e) => changePixelStyle(e))
+            pixel.addEventListener('mouseover', (e) => changePixelStyle(e));
+            
         }
         container.appendChild(pixelRow);
-
     }
 }
 makeContainer(count);
+
 
