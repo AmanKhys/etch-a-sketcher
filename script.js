d6073b04a08b1777dad7e366f1f896745b56c258
AmanKhys <amankhys2112@gmail.com>
2022-12-22 20:05:13 +0530

    change color of pixel to white on event 'mouseover'

--- script.js
+++ script.js
@@ -4,16 +4,31 @@ const header = document.getElementById('header');
 //
 
 
-//make pixel container
-let pixelCount =16;
-for(let i = 0; i<pixelCount;i++){
-    const pixelRow = document.createElement('div');
-    pixelRow.classList.add('pixel-row-style')
-    for(let j = 0; j<pixelCount; j++){
-        const pixel = document.createElement('div');
-        pixel.classList.add('pixel-style');
-        pixelRow.appendChild(pixel);
-    }
-    container.appendChild(pixelRow);
+let count =16;
 
+//change pixel style function
+function changePixelStyle(e){
+    console.log(`entered styles`);
+    const pixel = e.target;
+    pixel.classList.add('pixel-move-style');
 }
+
+
+//make pixel container function
+function makeContainer(pixelCount){
+    console.log(`pixel count is ${pixelCount}`);
+    for(let i = 0; i<pixelCount;i++){
+        const pixelRow = document.createElement('div');
+        pixelRow.classList.add('pixel-row-style')
+        for(let j = 0; j<pixelCount; j++){
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel-style');
+            pixelRow.appendChild(pixel);
+            pixel.addEventListener('mouseover', (e) => changePixelStyle(e))
+        }
+        container.appendChild(pixelRow);
+
+    }
+}
+makeContainer(count);
+
